4531d22ca845ba5992f776ee2048d7c6a73a4a64
gyh <535374674@qq.com>
2020-04-26 17:34:35 +0800

    test_3_3

---
+++ test_3_3/SortTestHelper.h
@@ -0,0 +1,47 @@
+﻿#ifndef SELECTIONSORT_SORTTESTHELPER_H
+#define SELECTIONSORT_SORTTESRHELPER_H
+#include <iostream>
+#include <ctime>
+#include <cassert>
+#include <string>
+using namespace std;
+
+namespace SortTestHelper{
+	int* generateRandArray(int n, int rangeL, int rangeR){
+
+		assert(rangeL <= rangeR);
+
+		int *arr = new int[n];
+		srand(time(NULL));
+		for (int i = 0; i < n; i++)
+			arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+		return arr;
+	}
+
+	template <typename T>
+	bool isSorted(T arr[], int n){
+		for (int i = 0; i < n - 1; i++)
+			if (arr[i]>arr[i + 1])
+				return false;
+		return true;
+	}
+
+	template<typename T>
+	void testSort(const string &sortName, void(*sort)(T[], int), T arr[], int n){
+		clock_t startTime = clock();
+		sort(arr, n);
+		clock_t endTime = clock();
+		cout << sortName << ": " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+		assert(isSorted(arr, n));
+		return;
+	}
+
+	int* copyIntArray(int a[], int n){
+		int* arr = new int[n];
+		copy(a, a + n, arr);
+		return arr;
+	}
+
+}
+
+#endif
---
+++ test_3_3/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include"Heap.h"
+#include"SortTestHelper.h"
+
+using namespace  std;
+
+template<typename T>
+void heapSort1(T arr[], int n){
+	MaxHeap<T> maxheap = MaxHeap<T>(n);
+	for (int i = 0; i < n; i++){
+		maxheap.insert(arr[i]);             //依次将数组中的元素放入到最大堆当中
+	}
+	for (int i = n - 1; i >= 0; i--)
+		arr[i] = maxheap.extractMax();
+}
+
+template<typename T>
+void heapSort2(T arr[], int n)              //第二类堆排序
+{
+	MaxHeap<T> maxheap = MaxHeap<T>(arr, n);
+	for (int i = n - 1; i >= 0; i--)
+		arr[i] = maxheap.extractMax();
+}
+
+int main()
+{
+	int n = 10000;
+	cout << "Test for Random Array,size = " << n << ",random range [0, " << n << "]" << endl;
+	int *arr1 = SortTestHelper::generateRandArray(n, 0, n);
+	int *arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+	SortTestHelper::testSort("Heap Sort1", heapSort1, arr1, n);
+	SortTestHelper::testSort("Heap Sort2", heapSort2, arr2, n);
+
+	delete[] arr1;
+	delete[] arr2;
+
+	system("pause");
+	return 0;
+}
